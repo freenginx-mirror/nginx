5bbf1cda48d90dca9e6be31bfddf687f504d7644
Maxim Dounin <mdounin@mdounin.ru>
2026-03-10 03:50:52 +0300

    Fixed clcf->auto_redirect in uwsgi_pass/scgi_pass with variables.
    Previously, clcf->auto_redirect was only set (for locations ending with "/")
    when uwsgi_pass and scgi_pass were used without variables, but not with
    variables.
    
    For proxy_pass and fastcgi_pass this was fixed in 2989:dff9764eaca2 (0.8.7).
    It was not, however, fixed in ngx_http_uwsgi_module (as imported later,
    in 3541:21452748d165, 0.8.40) and ngx_http_scgi_module (introduced shortly
    after, in 3637:d656caa72ec9, 0.8.42).
    
    The fix is mostly identical to the one in 2989:dff9764eaca2 (0.8.7) and
    ensures that clcf->auto_redirect is set both with and without variables.

--- src/http/modules/ngx_http_scgi_module.c
+++ src/http/modules/ngx_http_scgi_module.c
@@ -1921,7 +1921,12 @@ ngx_http_scgi_pass(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     }
 
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+
     clcf->handler = ngx_http_scgi_handler;
+
+    if (clcf->name.len && clcf->name.data[clcf->name.len - 1] == '/') {
+        clcf->auto_redirect = 1;
+    }
 
     value = cf->args->elts;
 
@@ -1956,10 +1961,6 @@ ngx_http_scgi_pass(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     scf->upstream.upstream = ngx_http_upstream_add(cf, &u, 0);
     if (scf->upstream.upstream == NULL) {
         return NGX_CONF_ERROR;
-    }
-
-    if (clcf->name.len && clcf->name.data[clcf->name.len - 1] == '/') {
-        clcf->auto_redirect = 1;
     }
 
     return NGX_CONF_OK;
